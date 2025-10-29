b71c0af0af64b205f8f12828f041ed0063f4474b
mitpatil07 <mitesh8767@gmail.com>
2025-10-29 09:42:32 +0530

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,8 +175,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "https://www.whatyouwear.store",
     "https://whatyouwear.store",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://localhost:5173", 
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
