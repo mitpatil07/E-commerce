9f6a652767a91a9e08ba72666258af9935533c94
mitpatil07 <mitesh8767@gmail.com>
2025-10-29 23:43:58 +0530

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dj_database_url
-
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 # ========================
 # ✅ Security
 # ========================
